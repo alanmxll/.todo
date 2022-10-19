5abfc2bc162ca516ed3a1d620c24fbf36930b458
Alan Maxwell <alanmaxwellantunes@gmail.com>
2022-10-19 20:08:51 -0300

    chore: add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-
 import styles from './App.module.css';
+import { Header } from './components/Header';
 
 import './global.css';
 
 export function App() {
   return (
-    <div className={styles.wrapper}></div>
+    <div className={styles.wrapper}>
+      <Header />
+    </div>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import todoLogo from '../assets/logo.svg';
+
+import styles from './Header.module.css';
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={todoLogo} alt=".todo logo" />
+    </header>
+  )
+}
