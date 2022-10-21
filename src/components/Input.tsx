fcb7a5f8c6142f8a03ca982cb3a28975f7959429
Alan Maxwell <alanmaxwellantunes@gmail.com>
2022-10-21 15:00:01 -0300

    chore: add input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,17 @@
 import styles from './Input.module.css';
+interface InputProps {
+  onChangeTaskDescription: (description: string) => void;
+}
 
-export function Input() {
+export function Input({ onChangeTaskDescription }: InputProps) {
   return (
     <input
       className={styles.input}
       type="text"
       placeholder='Adicione uma nova tarefa'
+      onChange={(event) => {
+        onChangeTaskDescription(event.target.value);
+      }}
     />
   )
 }
